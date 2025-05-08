16ab4bf3d1ae1179788b8a0efb5fe3a68c5209c1
Rael Garcia <rael@redhat.com>
2025-05-08 10:12:18 +0200

    feat: conditional dynamodb table creation
    Signed-off-by: Rael Garcia <rael@redhat.com>

--- variables.tf
+++ variables.tf
@@ -1,3 +1,9 @@
 variable "aws_account_name" {
   type = string
 }
+
+variable "dynamodb_lock" {
+  description = "Deploy a DynamoDB table for state locking"
+  type        = bool
+  default     = false
+}
